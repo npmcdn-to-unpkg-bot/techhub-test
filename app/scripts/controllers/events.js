f3da48166c3d19a1b1a1cd9f7ca8599f3072d67f
Ricardo <riboher@gmail.com>
2016-06-29 16:30:01 +0200

    Adapted promise call

---
+++ app/scripts/controllers/events.js
@@ -0,0 +1,9 @@
+angular.module('techhubApp').controller('EventCtrl',function(events){
+    var vm = this;
+    vm.events = [];
+
+    events.then(function(response){
+        vm.events = response.data;
+        console.log(vm.events);
+    });
+});
